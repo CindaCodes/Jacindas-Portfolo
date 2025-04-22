439d147f59dba1f28baf458d35b218d639b1e26e
cinda87 <j.c.bietz@gmail.com>
2025-04-21 20:31:30 -0600

    Fix Font Awesome import

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "./Projects.css";
 
 const projects = [
