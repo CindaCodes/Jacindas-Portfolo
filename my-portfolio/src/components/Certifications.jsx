e2038c466e9c82ffa6d1a57fed2da23aa02f9b5f
cinda87 <j.c.bietz@gmail.com>
2026-03-19 18:20:43 -0400

    chore: remove cert links

--- my-portfolio/src/components/Certifications.jsx
+++ my-portfolio/src/components/Certifications.jsx
@@ -9,7 +9,6 @@ const certifications = [
     date: "November 2024",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/226/807/original/226807.png?1743082879",
-    link: "https://www.shecodes.io/cohorts/2256/projects/2475335?_gl=1*fcgub5*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Basics Add-on",
@@ -17,7 +16,6 @@ const certifications = [
     date: "November 2024",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/226/945/original/226945.png?1743075193",
-    link: "https://www.shecodes.io/cohorts/2245/projects/2470800?_gl=1*1yiyk38*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Python Basics",
@@ -25,7 +23,6 @@ const certifications = [
     date: "December 2024",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/227/343/original/227343.png?1743075321",
-    link: "https://www.shecodes.io/cohorts/2261/projects/2505714?_gl=1*1kbady4*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Plus",
@@ -33,7 +30,6 @@ const certifications = [
     date: "January 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/227/290/original/227290.png?1743075308",
-    link: "https://www.shecodes.io/cohorts/2270/projects/2515108?_gl=1*a1gvbf*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Plus Add-on",
@@ -41,7 +37,6 @@ const certifications = [
     date: "January 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/230/602/original/230602.png?1743076257",
-    link: "https://www.shecodes.io/cohorts/2331/projects/2525147?_gl=1*1tuiu32*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Python Advanced",
@@ -49,7 +44,6 @@ const certifications = [
     date: "January 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/231/335/original/231335.png?1743076447",
-    link: "https://www.shecodes.io/cohorts/2346/projects/2535521?_gl=1*udvhhc*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Plus AI",
@@ -57,7 +51,6 @@ const certifications = [
     date: "February 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/232/045/original/232045.png?1743076637",
-    link: "https://www.shecodes.io/cohorts/2401/projects/2544817?_gl=1*1ev9t0d*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Python AI",
@@ -73,7 +66,6 @@ const certifications = [
     date: "March 2024",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/232/770/original/232770.png?1743076758",
-    link: "https://www.shecodes.io/cohorts/2428/projects/2570517?_gl=1*4p102r*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes Responsive Add-on",
@@ -81,7 +73,6 @@ const certifications = [
     date: "March 2024",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/235/100/original/235100.png?1743077130",
-    link: "https://www.shecodes.io/cohorts/2453/projects/2577221?_gl=1*dv6hyl*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes React",
@@ -89,7 +80,6 @@ const certifications = [
     date: "April 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/235/697/original/235697.png?1743806714",
-    link: "https://www.shecodes.io/cohorts/2490/projects/2597513?_gl=1*bbneyf*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
   {
     title: "SheCodes React",
@@ -97,7 +87,6 @@ const certifications = [
     date: "April 2025",
     image:
       "https://s3.amazonaws.com/shecodesio-production/students/certificates/000/237/402/original/237402.png?1744908521",
-    link: "https://www.shecodes.io/cohorts/2527/projects/2607890?_gl=1*r1srek*_gcl_au*MTI1MjQ5MTg5NS4xNzM4MDkyMzUzLjM1OTQ1ODkzNy4xNzQ0OTk2MTUyLjE3NDQ5OTYyMTA.",
   },
 ];
 
